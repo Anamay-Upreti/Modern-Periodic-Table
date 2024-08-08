5e486e72d09f43962fc4c64fdba80e5c60e2b2bb
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-08 23:34:21 +0530

    started-navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-[#002240] p-4  mx-4 md:mx-8 w-[1422px] h-20 rounded-3xl absolute top-4">
+      
+    </nav>
+  );
+};
+
+export default Navbar;
