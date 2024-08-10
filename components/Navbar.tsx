c11f3253f83c89e150a713ee5f7acb81c820597a
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-08-10 23:54:21 +0530

    added-nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,8 +2,13 @@ import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="bg-[#002240] p-4  mx-4 md:mx-8 w-[1422px] h-20 rounded-3xl absolute top-4">
-      
+    <nav className="bg-[#002240] p-4  mx-4 md:mx-8 w-[1422px] h-20 rounded-3xl absolute top-4 backdrop-blur-2xl bg-opacity-50  ">
+      <ul className='flex items-center justify-center space-x-24 font-semi-bold text-4xl'>
+        <li className=' text-orange-500 '>Home</li>
+        <li className=' text-orange-500 '>Reaction</li>
+        <li className=' text-orange-500 '>Make Table</li>
+        
+      </ul>
     </nav>
   );
 };
