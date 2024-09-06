c6cf52cd35eac701cd975d6d0a34c7126258d0e6
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-09-06 21:50:05 +0530

    improvedui

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,22 +9,22 @@ const Navbar = () => {
         <ul className="flex items-center justify-center space-x-24 font-semi-bold text-4xl font-ocr">
           <img src="/chem.png" className="w-20 h-12" />
           
-          {/* List Items with Glowing Hover Effect */}
-          <li className="text-orange-500 cursor-pointer hover:text-orange-300 hover:shadow-[0_0_10px_#ff7518]">
+          {/* List Items with Text Glow Effect */}
+          <li className="text-orange-500 cursor-pointer hover:text-orange-300 hover:[text-shadow:0_0_10px_#ff7518,0_0_20px_#ff7518,0_0_30px_#ff7518]">
             Home
           </li>
-          <li className="text-orange-500 cursor-pointer hover:text-orange-300 hover:shadow-[0_0_10px_#ff7518]">
+          <li className="text-orange-500 cursor-pointer hover:text-orange-300 hover:[text-shadow:0_0_10px_#ff7518,0_0_20px_#ff7518,0_0_30px_#ff7518]">
             Reaction
           </li>
-          <li className="text-orange-500 cursor-pointer hover:text-orange-300 hover:shadow-[0_0_10px_#ff7518]">
+          <li className="text-orange-500 cursor-pointer hover:text-orange-300 hover:[text-shadow:0_0_10px_#ff7518,0_0_20px_#ff7518,0_0_30px_#ff7518]">
             Make-Table
           </li>
 
           {/* Buttons with Glowing Hover Effect */}
-          <button className="flex text-white text-2xl bg-orange-500 w-28 h-10 text-center items-center justify-center rounded-full hover:bg-orange-400 hover:shadow-[0_0_10px_#ff7518]">
+          <button className="flex text-white text-2xl bg-orange-500 w-28 h-10 text-center items-center justify-center rounded-full hover:bg-orange-400 hover:[box-shadow:0_0_10px_#ff7518,0_0_20px_#ff7518]">
             Play <PiPlay />
           </button>
-          <button className="flex text-white text-xl bg-orange-500 w-28 h-10 text-center items-center justify-center rounded-full hover:bg-orange-400 hover:shadow-[0_0_10px_#ff7518]">
+          <button className="flex text-white text-xl bg-orange-500 w-28 h-10 text-center items-center justify-center rounded-full hover:bg-orange-400 hover:[box-shadow:0_0_10px_#ff7518,0_0_20px_#ff7518]">
             Register
           </button>
         </ul>
