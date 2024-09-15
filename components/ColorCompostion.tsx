d87a592de9c416116b1d1e96c9e15e96bfed3994
Anamay-Upreti <rcupretidgc13@gmail.com>
2024-09-15 22:27:52 +0530

    adjustments

--- components/ColorCompostion.tsx
+++ components/ColorCompostion.tsx
@@ -6,33 +6,31 @@ const groupColors = {
   metalloids: "bg-yellow-500", // Example color for Metalloids
 };
 
-export default function Elements() {
+export default function ColorComposition() {
   return (
-    <div className="w-full flex justify-center mt-10">
-      <div className="bg-blue-100 border border-blue-500 p-6 rounded-lg">
-        <table className="table-auto">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">Group</th>
-              <th className="px-4 py-2">Color</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border px-4 py-2">Alkali</td>
-              <td className={`border px-4 py-2 ${groupColors.alkali}`}></td>
-            </tr>
-            <tr>
-              <td className="border px-4 py-2">Non Metal</td>
-              <td className={`border px-4 py-2 ${groupColors.nonMetal}`}></td>
-            </tr>
-            <tr>
-              <td className="border px-4 py-2">Metalloids</td>
-              <td className={`border px-4 py-2 ${groupColors.metalloids}`}></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+    <div className="absolute top-10 left-10 bg-blue-100 border border-blue-500 p-6 rounded-lg shadow-lg">
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Group</th>
+            <th className="px-4 py-2">Color</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border px-4 py-2">Alkali</td>
+            <td className={`border px-4 py-2 ${groupColors.alkali}`}></td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Non Metal</td>
+            <td className={`border px-4 py-2 ${groupColors.nonMetal}`}></td>
+          </tr>
+          <tr>
+            <td className="border px-4 py-2">Metalloids</td>
+            <td className={`border px-4 py-2 ${groupColors.metalloids}`}></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
